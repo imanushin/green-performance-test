a055ff4335974cf67c0f8969470bd33a41429140
Igor Manushin <igor.manushin@pm.me>
2024-03-12 20:37:23 +0000

    add more benchmarks

--- src/jmh/java/com/github/imanushin/green/performance/benchmarks/ToGreenOrNotToGreen.kt
+++ src/jmh/java/com/github/imanushin/green/performance/benchmarks/ToGreenOrNotToGreen.kt
@@ -1,7 +1,13 @@
 package com.github.imanushin.green.performance.benchmarks
 
 import com.github.imanushin.green.performance.AsyncIncrementer
-import org.openjdk.jmh.annotations.*
+import com.github.imanushin.green.performance.AsyncIncrementerWithNestedMethods
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.Mode
+import org.openjdk.jmh.annotations.Threads
+import org.openjdk.jmh.annotations.Warmup
 import org.openjdk.jmh.infra.Blackhole
 
 @BenchmarkMode(Mode.All)
@@ -27,5 +33,25 @@ open class ToGreenOrNotToGreen {
     @Benchmark
     fun greenThread(blackhole: Blackhole) {
         blackhole.consume(AsyncIncrementer.greenThread())
+    }
+
+    @Benchmark
+    fun coroutineClassicWithNestedMethods(blackhole: Blackhole) {
+        blackhole.consume(AsyncIncrementerWithNestedMethods.coroutineClassic())
+    }
+
+    @Benchmark
+    fun coroutineGreenWithNestedMethods(blackhole: Blackhole) {
+        blackhole.consume(AsyncIncrementerWithNestedMethods.coroutineGreen())
+    }
+
+    @Benchmark
+    fun classicThreadWithNestedMethods(blackhole: Blackhole) {
+        blackhole.consume(AsyncIncrementerWithNestedMethods.classicThread())
+    }
+
+    @Benchmark
+    fun greenThreadWithNestedMethods(blackhole: Blackhole) {
+        blackhole.consume(AsyncIncrementerWithNestedMethods.greenThread())
     }
 }
